a13b24838addaa813dfcedb58521029ff66b2dc9
StefanPej <79942683+StefanPej@users.noreply.github.com>
2022-04-26 14:51:13 +1000

    i should commit  more

---
+++ wordcloud/flaskapp.py
@@ -0,0 +1,43 @@
+import mysql.connector
+from flask import Flask, request, g
+from flask_restful import Api, Resource, reqparse
+import json
+
+DB_ENDPOINT = "tp22main.ckwpvpzfxlw0.us-west-1.rds.amazonaws.com"
+DB_USER = "admin"
+DB_PASS = "12345678"
+DB = "main"
+
+db = mysql.connector.connect(
+    host=DB_ENDPOINT,
+    user=DB_USER,
+    password=DB_PASS,
+    database=DB
+)
+
+cursor = db.cursor()
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Users(Resource):
+
+    def get(self):
+
+        cursor.execute("SELECT * FROM wordcloud")
+        data = cursor.fetchall()
+        clean = []
+
+        for row in data:
+            vocab = json.loads(row[2])
+            wordcounts = []
+            for k, v in vocab.items():
+                wordcounts.append({"word": k, "count": v})
+            clean.append({"source": row[0], "handle": row[1], "wordcounts": wordcounts})
+        return clean, 200
+
+
+api.add_resource(Users, "/users")
+
+app.run()
--- wordcloud/test_wc/app.js
+++ wordcloud/test_wc/app.js
@@ -1,3 +1,397 @@
-const DATA = d3.json("\\fake_news_counts2.json");
+test = [
+  { word: "look", count: 4151 },
+  { word: "way", count: 8809 },
+  { word: "officials", count: 3933 },
+  { word: "family", count: 4765 },
+  { word: "speech", count: 4039 },
+  { word: "americans", count: 6780 },
+  { word: "watch", count: 6602 },
+  { word: "took", count: 4557 },
+  { word: "donald", count: 17671 },
+  { word: "com", count: 8508 },
+  { word: "gop", count: 4341 },
+  { word: "may", count: 7696 },
+  { word: "put", count: 4421 },
+  { word: "york", count: 4417 },
+  { word: "states", count: 10191 },
+  { word: "women", count: 7396 },
+  { word: "attack", count: 4293 },
+  { word: "left", count: 5669 },
+  { word: "want", count: 7329 },
+  { word: "could", count: 10246 },
+  { word: "state", count: 13463 },
+  { word: "information", count: 4265 },
+  { word: "house", count: 11113 },
+  { word: "says", count: 7102 },
+  { word: "old", count: 4694 },
+  { word: "week", count: 4954 },
+  { word: "gun", count: 4139 },
+  { word: "according", count: 8076 },
+  { word: "world", count: 7428 },
+  { word: "president", count: 27715 },
+  { word: "many", count: 9945 },
+  { word: "america", count: 11185 },
+  { word: "investigation", count: 4117 },
+  { word: "presidential", count: 5744 },
+  { word: "school", count: 4059 },
+  { word: "report", count: 4638 },
+  { word: "statement", count: 4114 },
+  { word: "country", count: 9229 },
+  { word: "republicans", count: 7401 },
+  { word: "twitter", count: 11721 },
+  { word: "see", count: 7468 },
+  { word: "washington", count: 5737 },
+  { word: "russian", count: 4539 },
+  { word: "republican", count: 9325 },
+  { word: "court", count: 5320 },
+  { word: "also", count: 15403 },
+  { word: "far", count: 4054 },
+  { word: "time", count: 13844 },
+  { word: "today", count: 4909 },
+  { word: "help", count: 4357 },
+  { word: "first", count: 10603 },
+  { word: "know", count: 10383 },
+  { word: "fact", count: 6279 },
+  { word: "order", count: 4064 },
+  { word: "government", count: 9202 },
+  { word: "bill", count: 6417 },
+  { word: "since", count: 6359 },
+  { word: "something", count: 5001 },
+  { word: "administration", count: 5769 },
+  { word: "via", count: 12776 },
+  { word: "russia", count: 5970 },
+  { word: "called", count: 6431 },
+  { word: "clinton", count: 19173 },
+  { word: "use", count: 4347 },
+  { word: "news", count: 14620 },
+  { word: "democrats", count: 4532 },
+  { word: "reported", count: 3908 },
+  { word: "2016", count: 6262 },
+  { word: "national", count: 7209 },
+  { word: "man", count: 6618 },
+  { word: "things", count: 4649 },
+  { word: "press", count: 4625 },
+  { word: "https", count: 4510 },
+  { word: "co", count: 4443 },
+  { word: "case", count: 4906 },
+  { word: "said", count: 33763 },
+  { word: "would", count: 23562 },
+  { word: "year", count: 10221 },
+  { word: "story", count: 5866 },
+  { word: "got", count: 4820 },
+  { word: "never", count: 6320 },
+  { word: "law", count: 7565 },
+  { word: "war", count: 4938 },
+  { word: "need", count: 5086 },
+  { word: "actually", count: 4951 },
+  { word: "fox", count: 5396 },
+  { word: "cnn", count: 4731 },
+  { word: "u", count: 9720 },
+  { word: "campaign", count: 11107 },
+  { word: "united", count: 8011 },
+  { word: "point", count: 4058 },
+  { word: "yet", count: 3819 },
+  { word: "last", count: 8227 },
+  { word: "fbi", count: 5802 },
+  { word: "2017", count: 4461 },
+  { word: "great", count: 4301 },
+  { word: "saying", count: 5162 },
+  { word: "people", count: 26570 },
+  { word: "another", count: 6489 },
+  { word: "money", count: 4846 },
+  { word: "images", count: 4415 },
+  { word: "much", count: 7135 },
+  { word: "person", count: 3852 },
+  { word: "department", count: 5583 },
+  { word: "real", count: 3900 },
+  { word: "mr", count: 3940 },
+  { word: "2", count: 4211 },
+  { word: "get", count: 10833 },
+  { word: "children", count: 4052 },
+  { word: "policy", count: 3811 },
+  { word: "believe", count: 4327 },
+  { word: "public", count: 6775 },
+  { word: "supporters", count: 3817 },
+  { word: "john", count: 4001 },
+  { word: "without", count: 3896 },
+  { word: "come", count: 5062 },
+  { word: "black", count: 7731 },
+  { word: "nothing", count: 4271 },
+  { word: "let", count: 4518 },
+  { word: "police", count: 9110 },
+  { word: "party", count: 7950 },
+  { word: "two", count: 9114 },
+  { word: "read", count: 4249 },
+  { word: "thing", count: 4534 },
+  { word: "getty", count: 4141 },
+  { word: "place", count: 3836 },
+  { word: "media", count: 11704 },
+  { word: "however", count: 4331 },
+  { word: "muslim", count: 3941 },
+  { word: "us", count: 14678 },
+  { word: "good", count: 5516 },
+  { word: "take", count: 6845 },
+  { word: "went", count: 4492 },
+  { word: "hillary", count: 14122 },
+  { word: "years", count: 8852 },
+  { word: "told", count: 9122 },
+  { word: "like", count: 18097 },
+  { word: "rights", count: 4007 },
+  { word: "featured", count: 8260 },
+  { word: "one", count: 24531 },
+  { word: "federal", count: 5415 },
+  { word: "office", count: 5742 },
+  { word: "asked", count: 5145 },
+  { word: "times", count: 5344 },
+  { word: "think", count: 8506 },
+  { word: "right", count: 10848 },
+  { word: "city", count: 4431 },
+  { word: "go", count: 6922 },
+  { word: "around", count: 4744 },
+  { word: "still", count: 5868 },
+  { word: "security", count: 5813 },
+  { word: "work", count: 5237 },
+  { word: "well", count: 8447 },
+  { word: "white", count: 13189 },
+  { word: "interview", count: 4095 },
+  { word: "pic", count: 6292 },
+  { word: "show", count: 6143 },
+  { word: "ever", count: 4373 },
+  { word: "say", count: 8680 },
+  { word: "000", count: 5249 },
+  { word: "million", count: 5144 },
+  { word: "part", count: 4966 },
+  { word: "post", count: 4974 },
+  { word: "really", count: 6214 },
+  { word: "made", count: 9118 },
+  { word: "three", count: 4051 },
+  { word: "going", count: 9796 },
+  { word: "candidate", count: 4437 },
+  { word: "including", count: 4503 },
+  { word: "matter", count: 4266 },
+  { word: "even", count: 14011 },
+  { word: "political", count: 7654 },
+  { word: "back", count: 8824 },
+  { word: "american", count: 11319 },
+  { word: "image", count: 9895 },
+  { word: "justice", count: 3932 },
+  { word: "group", count: 5677 },
+  { word: "day", count: 6908 },
+  { word: "nation", count: 4264 },
+  { word: "realdonaldtrump", count: 4372 },
+  { word: "video", count: 8338 },
+  { word: "election", count: 8934 },
+  { word: "vote", count: 5764 },
+  { word: "obama", count: 18797 },
+  { word: "every", count: 6101 },
+  { word: "course", count: 3811 },
+  { word: "though", count: 4060 },
+  { word: "woman", count: 4149 },
+  { word: "new", count: 14394 },
+  { word: "used", count: 4592 },
+  { word: "former", count: 7303 },
+  { word: "anti", count: 4321 },
+  { word: "1", count: 5219 },
+  { word: "democratic", count: 4901 },
+  { word: "long", count: 4636 },
+  { word: "make", count: 9254 },
+  { word: "support", count: 5963 },
+  { word: "percent", count: 4417 },
+  { word: "trump", count: 79300 },
+  { word: "life", count: 4022 },
+];
 
-console.log(DATA.vocab)
+function countMap(wordcount) {
+  let wordCountNew = {};
+  wordcount.forEach((word) => {
+    wordCountNew[word.word] = word.count;
+  });
+  return wordCountNew;
+}
+
+function wordsToDict(countMap, dictionary) {
+  for (let key in countMap) {
+    dictionary[key] = true;
+  }
+}
+
+function countMapToVect(countmap, vocabdict) {
+  let vect = [];
+  for (let word in vocabdict) {
+    vect.push(countmap[word]);
+  }
+  return vect;
+}
+
+function dotProd(vec1, vec2) {
+  let sum = 0;
+  for (let i = 0; i < vec1.length; i++) {
+    sum += vec1[i] * vec2[i];
+  }
+  return sum;
+}
+
+function magnitude(vec) {
+  let sum = 0;
+  for (let i = 0; i < vec.length; i++) {
+    sum += vec[i] * vec[i];
+  }
+  return Math.sqrt(sum);
+}
+
+function cosSim(vec1, vec2) {
+  return dotProd(vec1, vec2) / (magnitude(vec1) * magnitude(vec2));
+}
+
+function getSimilarity(wordcount1, wordcount2) {
+  let counts1 = countMap(wordcount1);
+  let counts2 = countMap(wordcount2);
+  let dict = {};
+  wordsToDict(counts1, dict);
+  wordsToDict(counts2, dict);
+  let vec1 = countMapToVect(counts1, dict);
+  let vec2 = countMapToVect(counts2, dict);
+  return cosSim(vec1, vec2);
+}
+
+console.log(getSimilarity(test, test));
+
+// Main function
+d3.json("http://ec2-100-27-46-58.compute-1.amazonaws.com/vocabs").then(
+  function (data) {
+    const counts = data[1][0].wordcounts;
+
+    var words = counts.map(function (d) {
+      return { text: d.word, size: d.count };
+    });
+
+    //console.log(words);
+
+    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+      width = 1000 - margin.left - margin.right,
+      height = 1000 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3
+      .select("#my_dataviz")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var svg2 = d3
+      .select("#my_dataviz2")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+    var layout = d3.layout
+      .cloud()
+      .size([width, height])
+      .words(words)
+      .padding(10)
+      .fontSize(function (d) {
+        return d.size / 400;
+      })
+      .rotate(0)
+      .on("end", draw);
+    layout.start();
+
+    // This function takes the output of 'layout' above and draw the words
+    // Better not to touch it. To change parameters, play with the 'layout' variable above
+    function draw(words) {
+      svg
+        .append("g")
+        .attr(
+          "transform",
+          "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")"
+        )
+        .selectAll("text")
+        .data(words)
+        .enter()
+        .append("text")
+        .style("font-size", function (d) {
+          return d.size + "px";
+        })
+        .attr("text-anchor", "middle")
+        .attr("transform", function (d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .attr("id", function (d) {
+          return "id" + d.text;
+        })
+        .text(function (d) {
+          return d.text;
+        })
+        .on("mouseover", function (d) {
+          //console.log(d.text);
+          var id = "id" + d.text;
+          console.log(id);
+          // Select current and make coloured
+          d3.select(this).style("fill", "red");
+          // Select matching element from wordcloud 2
+          d3.select("#my_dataviz2")
+            .select("g")
+            .selectAll("#" + id)
+            .style("fill", "red");
+        })
+        .on("mouseout", function (d) {
+          var id = "id" + d.text;
+          // Return current to base colour
+          d3.select(this).style("fill", "black");
+          // Return other to base colour
+          d3.select("#my_dataviz2")
+            .select("g")
+            .selectAll("#" + id)
+            .style("fill", "black");
+        });
+      svg2
+        .append("g")
+        .attr(
+          "transform",
+          "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")"
+        )
+        .selectAll("text")
+        .data(words)
+        .enter()
+        .append("text")
+        .style("font-size", function (d) {
+          return d.size + "px";
+        })
+        .attr("text-anchor", "middle")
+        .attr("transform", function (d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .attr("id", function (d) {
+          return "id" + d.text;
+        })
+        .text(function (d) {
+          return d.text;
+        })
+        .on("mouseover", function (d) {
+          var id = "id" + d.text;
+          // Select current and make coloured
+          d3.select(this).style("fill", "red");
+          // Select matching element from wordcloud 2
+          d3.select("#my_dataviz")
+            .select("g")
+            .selectAll("#" + id)
+            .style("fill", "red");
+        })
+        .on("mouseout", function (d) {
+          var id = "id" + d.text;
+          // Return current to base colour
+          d3.select(this).style("fill", "black");
+          // Return other to base colour
+          d3.select("#my_dataviz")
+            .select("g")
+            .selectAll("#" + id)
+            .style("fill", "black");
+        });
+    }
+  }
+);
